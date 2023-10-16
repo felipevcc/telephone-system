4aca9b006159a3dd7b1344bb8dd3d28e94366e3e
felipevcc <felipevillamizarc@gmail.com>
2023-10-16 10:08:08 -0500

    Angular project initialized

---
+++ Front/tc-gb-09/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tc-gb-09';
+}
