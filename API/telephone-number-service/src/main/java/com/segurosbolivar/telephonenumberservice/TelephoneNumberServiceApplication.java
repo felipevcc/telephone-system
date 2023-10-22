47cf470d510eeb738c339140373ffc2042827009
felipevcc <felipevillamizarc@gmail.com>
2023-10-22 16:50:07 -0500

    Initialization of customer and telephone number microservices

---
+++ API/telephone-number-service/src/main/java/com/segurosbolivar/telephonenumberservice/TelephoneNumberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.segurosbolivar.telephonenumberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelephoneNumberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelephoneNumberServiceApplication.class, args);
+	}
+
+}
